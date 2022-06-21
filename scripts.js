efb31e6518110d395ec733ba2b08dce1a81c9201
Harrand <harrysjh98@gmail.com>
2022-06-21 01:37:40 +0100

    - Removed all old crap

--- scripts.js
+++
@@ -1,8 +0,0 @@
-function getReadme($repo_name)
-{
-	$url = "https://github.com/Harrand/" + $repo_name + "/blob/master/README.md";
-	$.get('file_to_read.txt', function(data)
-	{
-		return data;
-	}, 'text');
-}
